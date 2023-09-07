40719a320574810e60bae53c91783221e2d6645e
Fabien Dev <fabiendevfront@gmail.com>
2023-09-07 14:24:42 +0200

    Dependencies installation and configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true },],
+    "indent": ["error", 4],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"]
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Router>
+            <Theme>
+                <App />
+            </Theme>
+        </Router>
+    </React.StrictMode>,
+);
