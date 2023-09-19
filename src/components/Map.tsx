1e845520d99b4411567eb440df4b9f8093bb0829
Fabien Dev <fabiendevfront@gmail.com>
2023-09-19 16:16:48 +0200

    Setting map, adding markers based on coordinates, adding a popup contain shelter info

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,33 @@
 import { MapProps } from "../types/types.ts";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = (props: MapProps) => {
-    console.log(props.shelters);
+
     return (
-        <h2>Maps</h2>
+        <MapContainer
+            center={[42.6461, 0.0711]}
+            zoom={8}
+            style={{ height: "700px", width: "100%" }}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {props.shelters.map((shelter, index) => (
+                <Marker
+                    key={index}
+                    position={[shelter.latitude, shelter.longitude]}
+                >
+                    <Popup>
+                        <div>
+                            <h3>{shelter.name}</h3>
+                            <p>Département: {shelter.department}</p>
+                            <p>Altitude: {shelter.altitude}</p>
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}
+        </MapContainer>
     );
 };
 
